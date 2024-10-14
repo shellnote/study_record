3d21ec6dd548517c0bb4e357d380a239a7042b97
kaizukasyun <3244kaizukashun@gmail.com>
2024-10-14 19:30:46 +0900

    urls.py edit

--- project/urls.py
+++ project/urls.py
@@ -25,3 +25,5 @@ urlpatterns = [
     path("record/", include("recordapp.urls")),
     
 ] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recordapp/urls.py
+++ recordapp/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path("post/<slug:slug>/", views.detail, name="detail")
     
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
